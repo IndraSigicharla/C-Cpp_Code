1d71e55ede80f859af9ddf1d5f9a675f8ee92524
IndraSigicharla <indhhra@gmail.com>
2020-12-13 12:22:15 +0530

    Minor changes.

--- Small-Code/chocolate.c
+++ Small-Code/chocolate.c
@@ -4,9 +4,14 @@
 int main()
 {
     int n, c, m, p, f;
-    scanf("%d%d%d", &n, &c, &m);
+    printf("Enter the money Bob has: ");
+    scanf(" %d", &n);
+    printf("\rEnter the cost of one chocolate: ");
+    scanf("%d", &c);
+    printf("\rEnter the number of wrappers for a free chocolate: ");
+    scanf("%d", &m);
     p = n/c;
     f = p/m;
-    printf("%d", p + f);
+    printf("The number of chocolates Bob can get is: %d", p + f);
     return 0;
 }
